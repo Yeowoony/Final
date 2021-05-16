f6f3d63f18a8609335ec3654fd045d7e0db4efdf
Yeowoony <dbd04009@naver.com>
2021-05-16 22:36:21 +0900

    change modelling File in Linux but error -> calloc error - possibly out of CPU RAM

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "snack models deep learning x 100"
+git commit -m "change modelling File in Linux but error -> calloc error - possibly out of CPU RAM "
 git push -u origin master
